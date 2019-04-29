cd92adc1ba1106ce579a044297d83b3f6397cb09
Sami Kohvakka <sami.kohvakka@live.com>
2019-04-29 18:26:20 +0300

    Run from command line

--- master/master.py
+++ master/master.py
@@ -1,6 +1,7 @@
 import json
 import random
 import time
+import sys
 import urllib
 from functools import partial
 #from japronto import Application
@@ -12,7 +13,7 @@ N_WORKERS = 8
 N_PROCESSES = 20
 
 def getLinks(page, paths, end, lang):
-    print("page = %s, n = %d"%(page, len(paths)))
+    #print("page = %s, n = %d"%(page, len(paths)))
     try:
         port = 3000 + random.randint(1,N_WORKERS)
         response = requests.get("http://127.0.0.1:" + str(port) + "/links?lang=" + lang + "&page=" + page)
@@ -26,7 +27,7 @@ def getLinks(page, paths, end, lang):
     for link in links:
         if link == end.lower():
             paths[link] = paths[page] + [link]
-            print("\n" + "->".join(paths[link]) + "\n")
+            print("\n" + "->".join(paths[link]))
             return [], paths[page] + [link]
         if (link not in paths) and (link != page):
             try:
@@ -54,18 +55,15 @@ def shortest_path(start, end, lang):
             Q = []
         data = p.map(partial(getLinks, paths=paths, end=end, lang=lang), [link for link in batch])
         p.close()
-        with open("data1.txt", "w") as f:
-            f.write(str(data))
         results = [x[1] for x in data if len(x[1]) > 0]
         if len(results) > 0:
-            print(results)
+            #print("\n Paths found:\n")
+            #print(results)
             cont = False
         tmp = [x[0] for x in data]
         Q += list(set([x for y in tmp for x in y]))
         s = set()
         Q = [x for x in Q if not (x in s or s.add(x))]
-    with open("results.txt", "w") as f:
-        f.write(str(results))
     return results
 
 
@@ -88,8 +86,28 @@ def main(request):
         return request.Response(text="Attribute error")
  """
 if __name__ == "__main__":
+    #print(sys.argv)
+    try: 
+        first = sys.argv[1]
+        last = sys.argv[2]
+        lang = sys.argv[3]
+        if lang not in ['fi', 'en', 'de']:
+            print("Known languages: fi, en, de")
+            exit(0)
+    except IndexError:
+        print("Usage: master.py <source> <target> <lang>")
+        exit(-1)
+
+    check = requests.get("https://{0}.wikipedia.org/wiki/{1}".format(lang, first))
+    if not check.ok:
+        print("First page is not a valid Wikipedia page")
+        exit(0)
+    check = requests.get("https://{0}.wikipedia.org/wiki/{1}".format(lang, last))
+    if not check.ok:
+        print("Last page is not a valid Wikipedia page")
+        exit(0)
     start = time.time()
-    results = shortest_path("cat", "australia", "en")
+    results = shortest_path(first, last, lang)
     print("\nIt took %d seconds."%(time.time() - start))
     
     #app = Application()
